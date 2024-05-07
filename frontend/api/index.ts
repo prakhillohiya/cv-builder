e7529fe0e26ba510c45b31a7a892f817dd5e4ea7
Prakhil Lohiya <prakhillohiya@gmail.com>
2024-05-07 13:24:57 +0530

    error fix

--- frontend/api/index.ts
+++ frontend/api/index.ts
@@ -1,9 +1,8 @@
 import express from 'express'
 
-const app = express();
+export const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
-module.exports = app;
