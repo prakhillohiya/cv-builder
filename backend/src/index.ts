525428bd8a98b8e78b331d3c7ab5cd136a975891
Prakhil Lohiya <prakhillohiya@gmail.com>
2024-05-08 18:20:33 +0530

    cors update

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,7 @@ import { IncomingHttpHeaders } from "http";
 const app = express();
 app.use(
   cors({
-    origin: "https://the-cv-builder.vercel.app",
+    origin: process.env.ALLOWED_ORIGINS?.split(" "),
     credentials: true,
   })
 );
