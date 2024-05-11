dcdc9292ea4f994066d70a7e333eb90e6319efcb
Prakhil Lohiya <prakhillohiya@gmail.com>
2024-05-11 11:39:14 +0530

    style fix

--- frontend/src/shared/components/ScrollableTabs.tsx
+++ frontend/src/shared/components/ScrollableTabs.tsx
@@ -26,7 +26,7 @@ const CustomTabPanel:React.FC<ICustomTabPanel>=({ index, children, value }) =>{
       hidden={value !== index}
       id={`simple-tabpanel-${index}`}
       aria-labelledby={`simple-tab-${index}`}
-      className="h-[calc(100vh - 244px)] overflow-auto"
+      className="h-[calc(100vh-244px)] overflow-auto"
 
     >
       {value === index && <Box>{children}</Box>}
